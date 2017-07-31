3a11f0ca906e4b00884ee8991196015f4ee99343
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-08-01 02:25:43 +1200

    adding game and rounds, code improvements

--- public/stop.js
+++ public/stop.js
@@ -9,31 +9,45 @@
         messagingSenderId: "97790153221"
     };
     firebase.initializeApp(config);
-    var databaseUserName, database = firebase.database();
-    
+    var  database = firebase.database(), roundNumber = 0, gameKey, databaseUserName, roundDatabaseUrl, roundDataUrl;
+    //DOM
     document.getElementById('gameBody').style.display = 'none';
-    document.getElementById('name').addEventListener('keypress', addDataToFirebase);
-    document.getElementById('electronics').addEventListener('keypress', addDataToFirebase);
-    document.getElementById('place').addEventListener('keypress', addDataToFirebase);
+    document.getElementById('name').addEventListener('keyup', addRoundData);
+    document.getElementById('electronics').addEventListener('keyup', addRoundData);
+    document.getElementById('place').addEventListener('keyup', addRoundData);
     document.getElementById('btnStop').addEventListener('click', btnStopClick);
     document.getElementById('btnStart').addEventListener('click', startGameClick);
 
-    database.ref('stop').on('value', function (snapshot) {
-        if (snapshot.val().stop) {
-            var inputs = document.getElementsByTagName('input');
-            for (var index = 0; index < inputs.length; ++index) {
-                inputs[index].disabled = true;
+    database.ref('games').orderByKey().limitToLast(1).on('child_added', function (snapshot) {
+        gameKey = snapshot.key;
+        roundDatabaseUrl = 'games/' + gameKey + '/' + roundNumber + '/';
+        database.ref(roundDatabaseUrl + 'stop').on('value', function (snapshot) {
+            if (snapshot.val()) {
+                var inputs = document.getElementsByTagName('input');
+                for (var index = 0; index < inputs.length; ++index) {
+                    inputs[index].disabled = snapshot.val().stop;
+                }
+                if (snapshot.val().stop) {
+                    roundNumber++;
+                }
             }
-        }
+        });
     });
-    function addDataToFirebase($event) {
-        database.ref(databaseUserName + '/' + [$event.srcElement.id]).set($event.srcElement.value);
+
+    function addRoundData($event) {
+        database.ref(roundDataUrl + [$event.srcElement.id]).set($event.srcElement.value);
     }
+
     function btnStopClick() {
-        database.ref('stop').set({stop: true});
+        database.ref(roundDatabaseUrl + '/stop').set({
+            stop: true
+        });
+        //Show STOP, clear screen to round 2
     }
+
     function startGameClick() {
         databaseUserName = document.getElementById('userName').value;
+        roundDataUrl = roundDatabaseUrl + databaseUserName + '/';
         document.getElementById('gameBody').style.display = 'block';
         document.getElementById('introduction').style.display = 'none';
     }
