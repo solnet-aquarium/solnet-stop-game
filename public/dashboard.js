eac1fe249f9e014d06939a50f13e78f2a287431b
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-08-04 02:18:00 +1200

    dashboard wip

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,20 +1,37 @@
-(function () {
-    'use strict';
-    var config = {
-        apiKey: "AIzaSyBX-6C-ju_GJEkvmqDd-gBEfIAP8_3fjZQ",
-        authDomain: "solnet-stop.firebaseapp.com",
-        databaseURL: "https://solnet-stop.firebaseio.com",
-        projectId: "solnet-stop",
-        storageBucket: "solnet-stop.appspot.com",
-        messagingSenderId: "97790153221"
-    };
-    firebase.initializeApp(config);
-    firebase.database().ref('games').orderByKey().limitToLast(1).on('child_added', function (snapshot) {
-        var game = snapshot.val();
+    angular.module('dashboardApp', []).controller('DashboardController', function ($scope) {
+        $scope.getPlayers = getPlayers;
+        this.loadResults = loadResults;
+        var config = {
+            apiKey: "AIzaSyBX-6C-ju_GJEkvmqDd-gBEfIAP8_3fjZQ",
+            authDomain: "solnet-stop.firebaseapp.com",
+            databaseURL: "https://solnet-stop.firebaseio.com",
+            projectId: "solnet-stop",
+            storageBucket: "solnet-stop.appspot.com",
+            messagingSenderId: "97790153221"
+        };
+        firebase.initializeApp(config);
 
-        for (var i = 0; i < 3; i++) {
-            console.log(snapshot.val()[i]);
+        function loadResults() {
+
+            firebase.database().ref('games').orderByKey().limitToLast(1).once('value', function (snapshot) {
+                var game = snapshot.val()[Object.keys(snapshot.val())];
+                if (game.players) {
+                    var arrayPlayers = Object.keys(game.players).map(function (key) {
+                        return {
+                            'rounds': game.players[key],
+                            'playerName': key
+                        };
+                    });
+                    $scope.$apply(function () {
+                        $scope.players = arrayPlayers;
+                    });
+                } else {
+                    $scope.players = [];
+                }
+            });
         }
-        console.log(Object.keys(snapshot.val()));
+
+        function getPlayers() {
+            return $scope.players;
+        }
     });
-}());
