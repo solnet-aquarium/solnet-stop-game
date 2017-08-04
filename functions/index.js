f47ea2774aaa2c5e4e9a519a848bc347b3301127
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2017-08-05 01:46:21 +1200

    fixing score calculation

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,44 @@ const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function (childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+
+        returnArr.push(item);
+    });
+    return returnArr;
+};
 exports.calculateScore = functions.database.ref('/games/{:gameId}/roundNumber')
     .onUpdate(function (event) {
         var eventSnapshot = event.data;
-        event.data.ref.parent.child('players').on('value', function (playersDataSnapshot) {
-            var players = playersDataSnapshot.val();
-            var playerArray = Object.keys(players).map(function (key) {
-                return {
-                    'rounds': players[key],
-                    'playerName': key
-                };
-            });
+        event.data.ref.parent.child('players').once('value', function (playersDataSnapshot) {
+            var playerArray = snapshotToArray(playersDataSnapshot);
             playerArray.forEach(function (player) {
                 var score = 0;
-                player.rounds.map(function (round) {
-                    score += Object.keys(round).length * 20;
+                var roundArray = [];
+                if (player.rounds.constructor !== Array) {
+                    roundArray = Object.keys(player.rounds).map(function (key) {
+                        return player.rounds[key];
+                    });
+                } else {
+                    roundArray = player.rounds;
+                }
+                roundArray.map(function (round) {
+                    var keys = Object.keys(round);
+                    for (var i = 0; i < keys.length; i++) {
+
+                        if (round[keys[i]].length > 2) {
+                            score += 25;
+                        }
+                    }
                 });
-                event.data.ref.parent.child('players/' + player.playerName).update({ score: score });
+                event.data.ref.parent.child('players/' + player.key).update({
+                    score: score
+                });
             }, this);
         });
     });
